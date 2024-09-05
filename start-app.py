07156539eb9c667be34fb9e4b8c34233cf6ce59d
Boris Lassort <163381+borislassort@users.noreply.github.com>
2024-09-05 16:40:25 +0200

    open browser on startup

--- start-app.py
+++ start-app.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import webbrowser
 
 def main():
     """Run administrative tasks."""
@@ -14,6 +15,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    webbrowser.open("http://127.0.0.1:8000/")
     execute_from_command_line(["manage.py", "runserver", "--noreload"])
 
 if __name__ == "__main__":
